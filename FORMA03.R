07cf8b93ec8175162e599df3a7a45ee441459d25
maximiliano araya <maximiliano.araya@usach.cl>
2022-01-21 19:01:25 -0300

    inicial

---
+++ FORMA03.R
@@ -0,0 +1,71 @@
+
+
+
+# FORMA 03
+
+# se importan librerias
+if (!require(ggplot2)){
+  install.packages("ggplot2", dependencies = TRUE )
+  require (ggplot2)
+}
+if (!require(ez)){
+  install.packages("ez", dependencies = TRUE )
+  require (ez)
+}
+
+if (!require(ggpubr)){
+  install.packages("ggpubr", dependencies = TRUE )
+  require (ggpubr)
+}
+
+if (!require(tidyr)){
+  install.packages("tidyr", dependencies = TRUE )
+  require (tidyr)
+}
+
+if (!require(emmeans)){
+  install.packages("emmeans", dependencies = TRUE )
+  require (emmeans)
+}
+
+
+#   ____                                          _               _ 
+#  |  _ \   _ __    ___    __ _   _   _   _ __   | |_    __ _    / |
+#  | |_) | | '__|  / _ \  / _` | | | | | | '_ \  | __|  / _` |   | |
+#  |  __/  | |    |  __/ | (_| | | |_| | | | | | | |_  | (_| |   | |
+#  |_|     |_|     \___|  \__, |  \__,_| |_| |_|  \__|  \__,_|   |_|
+#                         |___/                                 
+
+
+
+#                                                _               ____  
+#  _ __    _ __    ___    __ _   _   _   _ __   | |_    __ _    |___ \ 
+# | '_ \  | '__|  / _ \  / _` | | | | | | '_ \  | __|  / _` |     __) |
+# | |_) | | |    |  __/ | (_| | | |_| | | | | | | |_  | (_| |    / __/ 
+# | .__/  |_|     \___|  \__, |  \__,_| |_| |_|  \__|  \__,_|   |_____|
+# |_|                    |___/                                         
+
+
+
+#                                                _               _____ 
+#  _ __    _ __    ___    __ _   _   _   _ __   | |_    __ _    |___ / 
+# | '_ \  | '__|  / _ \  / _` | | | | | | '_ \  | __|  / _` |     |_ \ 
+# | |_) | | |    |  __/ | (_| | | |_| | | | | | | |_  | (_| |    ___) |
+# | .__/  |_|     \___|  \__, |  \__,_| |_| |_|  \__|  \__,_|   |____/ 
+# |_|                    |___/                                         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
